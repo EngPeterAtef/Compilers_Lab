0f1030b634acc72a2ae0b9d3207bd4e7a2cb8952
EngPeterAtef <peter_atef@stud.cu.edu.eg>
2024-04-04 10:46:26 +0200

    Implement regex to NFA conversion and visualization
    This commit introduces the capability to convert regular expressions into Non-deterministic Finite Automata (NFA) and visualize them. The main changes include the implementation of the NFA_CLASS in `regex_to_NFA.py`, which handles the conversion process, and the addition of functionality to output the NFA structure in both JSON format and as a graph visualization (`.gv` file and corresponding PDF).
    
    Key features added:
    - Tokenization and parsing of regular expressions to create an Abstract Syntax Tree (AST).
    - Construction of NFA from the AST for various regex constructs such as literals, character classes, quantifiers (*, +, ?), and alternation (|).
    - Serialization of the NFA to JSON format for easy storage and sharing.
    - Visualization of the NFA using Graphviz, allowing for a graphical representation of the state transitions.
    
    The `main.py` file has been updated to demonstrate the usage of the NFA_CLASS with a sample regex pattern. Additionally, the NFA visualization output has been simplified to focus on a specific regex pattern for clarity.
    
    This enhancement provides a foundation for further development in regex processing, enabling more complex pattern matching and analysis tasks.

--- src/regex_to_NFA.py
+++ src/regex_to_NFA.py
@@ -3,31 +3,36 @@ from parser_classes import *
 import json
 import matplotlib.pyplot as plt
 import networkx as nx
-from graph_visualize import GraphVisualize 
+from graph_visualize import GraphVisualize
 from json_serialize import JsonSerialize
+
+
 class State:
     pass
+
 
 @dataclass
 class Edge:
     from_: State
     to_: State
-    characters: set # of chars (literals, epsilon) and pairs of chars (for ranges)
+    characters: set  # of chars (literals, epsilon) and pairs of chars (for ranges)
+
 
 @dataclass
 class NFA:
     start: State
     accept: State
-    
+
     # All states of the NFA including the start and accept
-    states: list       # of State
+    states: list  # of State
     transitions: list  # of Edge
+
 
 class NFA_CLASS:
     def __init__(self, regex):
         if not self.check_regex(regex):
             raise ValueError(f"Invalid regular expression: {regex}")
-        
+
         self._regex = regex
         self._tokens = []
         self._ast = None
@@ -39,7 +44,7 @@ class NFA_CLASS:
         self.AST_to_NFA()
         self.nfa_to_json()
         self.visualize()
-    
+
     def check_regex(self, regex):
         """
         Function used to check if the regex is valid or not
@@ -50,7 +55,7 @@ class NFA_CLASS:
             print(f"Invalid regular expression: {regex}")
             return False
         return True
-    
+
     def tokenize(self):
         """
         Function used to tokenize the regex, and handle the escape char '\'
@@ -66,31 +71,31 @@ class NFA_CLASS:
             "[": TokenType.OPEN_SQ_BRACKET,
             "]": TokenType.CLOSED_SQ_BRACKET,
             "-": TokenType.DASH,
-            ".": TokenType.DOT
+            ".": TokenType.DOT,
         }
-        escape = '\\'
-        
+        escape = "\\"
+
         token_stream = []
         prev_char = None
-        
+
         for char in self._regex:
             if char == escape:
                 prev_char = char
                 continue
-            
+
             if char in character_map and prev_char != escape:
                 token_stream.append(Token(character_map[char], char))
             else:
                 token_stream.append(Token(TokenType.LITERAL, char))
 
             prev_char = char
-        
+
         self._tokens = token_stream
-    
+
     def parse(self):
         """
         Function used to create the Abstract Syntax Tree (AST) of the regex using the following grammar
-        
+
         regex-expression        -> or-expression
         or-expression           -> sequence-expression (OR sequence-expression)*
         sequence-expression     -> quantified-expression (quantified-expression)*
@@ -104,8 +109,7 @@ class NFA_CLASS:
         """
         expression, _ = parse_regex(self._tokens, 0)
         self._ast = expression
-    
-    
+
     def construct_nfa(self, node):
         if isinstance(node, OrAstNode):
             return self.construct_or_nfa(node)
@@ -115,12 +119,14 @@ class NFA_CLASS:
             return self.construct_star_plus_nfa(node)
         elif isinstance(node, QuestionMarkAstNode):
             return self.construct_question_mark_nfa(node)
-        elif isinstance(node, LiteralCharacterAstNode) or isinstance(node, CharacterClassAstNode):
+        elif isinstance(node, LiteralCharacterAstNode) or isinstance(
+            node, CharacterClassAstNode
+        ):
             return self.construct_literal_character_class_nfa(node)
-    
+
     def construct_or_nfa(self, node):
         # If we have regex A|B
-        
+
         # Get the NFAs representing A and B
         left_nfa = self.construct_nfa(node.left)
         right_nfa = self.construct_nfa(node.right)
@@ -128,143 +134,141 @@ class NFA_CLASS:
         # Create 2 new states, start and accept for the full NFA
         start = State()
         accept = State()
-        
+
         # Link the start state with both the start of A's NFA and B's NFA with an epsilon transitions
         start_transition_1 = Edge(from_=start, to_=left_nfa.start, characters={EPSILON})
-        start_transition_2 = Edge(from_=start, to_=right_nfa.start, characters={EPSILON})
-        
+        start_transition_2 = Edge(
+            from_=start, to_=right_nfa.start, characters={EPSILON}
+        )
+
         # Link the accept state with both the accept of A's NFA and B's NFA with an epsilon transitions
-        final_transition_1 = Edge(from_=left_nfa.accept, to_=accept, characters={EPSILON})
-        final_transition_2 = Edge(from_=right_nfa.accept, to_=accept, characters={EPSILON})
-        
+        final_transition_1 = Edge(
+            from_=left_nfa.accept, to_=accept, characters={EPSILON}
+        )
+        final_transition_2 = Edge(
+            from_=right_nfa.accept, to_=accept, characters={EPSILON}
+        )
+
         # Finalize the Or NFA and return it
-        states = [
-            *left_nfa.states,
-            *right_nfa.states,
-            start,
-            accept
-        ]
+        states = [*left_nfa.states, *right_nfa.states, start, accept]
         transitions = [
             *left_nfa.transitions,
             *right_nfa.transitions,
-            start_transition_1, start_transition_2,
-            final_transition_1, final_transition_2
+            start_transition_1,
+            start_transition_2,
+            final_transition_1,
+            final_transition_2,
         ]
         return NFA(start, accept, states, transitions)
-    
+
     def construct_seq_nfa(self, node):
         # If we have regex AB
-        
+
         # Get the NFAs representing A and B
         left_nfa = self.construct_nfa(node.left)
         right_nfa = self.construct_nfa(node.right)
 
         # Link the two NFAs by connecting the accept state of A to the start state of B
-        final_transition = Edge(from_=left_nfa.accept, to_=right_nfa.start, characters={EPSILON})
+        final_transition = Edge(
+            from_=left_nfa.accept, to_=right_nfa.start, characters={EPSILON}
+        )
 
         # Finalize the Seq NFA and return it
-        states = [
-            *left_nfa.states,
-            *right_nfa.states
-        ]
-        transitions = [
-            *left_nfa.transitions,
-            *right_nfa.transitions,
-            final_transition
-        ]
+        states = [*left_nfa.states, *right_nfa.states]
+        transitions = [*left_nfa.transitions, *right_nfa.transitions, final_transition]
         return NFA(left_nfa.start, right_nfa.accept, states, transitions)
-    
+
     def construct_star_plus_nfa(self, node):
         # If we have regex A* or A+
         is_star = isinstance(node, StarAstNode)
-        
+
         # Get the NFA representing A
         left_nfa = self.construct_nfa(node.left)
-        
+
         # Create 2 new states, start and accept for the full NFA
         start = State()
         accept = State()
-        
+
         # Link the new start to the start of A's NFA
         start_transition_1 = Edge(from_=start, to_=left_nfa.start, characters={EPSILON})
 
         # For star nodes only, it also goes to the accept state directly to represent accepting an empty inputs
         start_transition_2 = Edge(from_=start, to_=accept, characters={EPSILON})
-        
+
         # Link the accept state with the accept of A's NFA
-        final_transition_1 = Edge(from_=left_nfa.accept, to_=accept, characters={EPSILON})
+        final_transition_1 = Edge(
+            from_=left_nfa.accept, to_=accept, characters={EPSILON}
+        )
 
         # To represent zero|one 'or more' we link the accept state of A with the new start state
-        final_transition_2 = Edge(from_=left_nfa.accept, to_=start, characters={EPSILON})
-        
+        final_transition_2 = Edge(
+            from_=left_nfa.accept, to_=start, characters={EPSILON}
+        )
+
         # Finalize the star or plus NFA and return it
-        states = [
-            *left_nfa.states,
-            start,
-            accept
-        ]
+        states = [*left_nfa.states, start, accept]
         transitions = [
             *left_nfa.transitions,
             start_transition_1,
-            final_transition_1, final_transition_2
+            final_transition_1,
+            final_transition_2,
         ]
         if is_star:
             transitions.append(start_transition_2)
-        
+
         return NFA(start, accept, states, transitions)
-    
+
     def construct_question_mark_nfa(self, node):
         # If we have regex A?
-        
+
         # Get the NFA representing A
         left_nfa = self.construct_nfa(node.left)
-        
+
         # Create 2 new states, start and accept for the full NFA
         start = State()
         accept = State()
-        
+
         # Link the new start to the start of A's NFA
         start_transition_1 = Edge(from_=start, to_=left_nfa.start, characters={EPSILON})
 
         # It also goes to the accept state directly to represent accepting an empty inputs
         start_transition_2 = Edge(from_=start, to_=accept, characters={EPSILON})
-        
+
         # Link the accept state with the accept of A's NFA
-        final_transition_1 = Edge(from_=left_nfa.accept, to_=accept, characters={EPSILON})
+        final_transition_1 = Edge(
+            from_=left_nfa.accept, to_=accept, characters={EPSILON}
+        )
 
         # Finalize the question mark NFA and return it
-        states = [
-            *left_nfa.states,
-            start,
-            accept
-        ]
+        states = [*left_nfa.states, start, accept]
         transitions = [
             *left_nfa.transitions,
-            start_transition_1, start_transition_2,
-            final_transition_1
+            start_transition_1,
+            start_transition_2,
+            final_transition_1,
         ]
-        
+
         return NFA(start, accept, states, transitions)
-    
+
     def construct_literal_character_class_nfa(self, node):
         # If we have regex 'x' for any character x or a character class [..]
-        
+
         is_literal = isinstance(node, LiteralCharacterAstNode)
         chars = {node.char} if is_literal else node._class
-        
+
         # Create 2 new states, start and accept for the full NFA
         start = State()
         accept = State()
-        
+
         # Single transition that goes from the starting to the accepting on the relevant characters
         final_transition = Edge(start, accept, chars)
-        
+
         # Finalize the literal and character class NFA and return it
         states = [start, accept]
         transitions = [final_transition]
-        
+
         return NFA(start, accept, states, transitions)
-    
+
     def AST_to_NFA(self):
         self._nfa = self.construct_nfa(self._ast)
 
@@ -273,15 +277,20 @@ class NFA_CLASS:
         json_serialize = JsonSerialize()
         self._nfa_json = json_serialize.nfa_json_serialize(nfa)
         del json_serialize
-        
-    def visualize(self, name='./nfa.gv'):
+
+    def visualize(self, name="./nfa.gv"):
         json_data = self._nfa_json
         graph_visualize = GraphVisualize()
-        if graph_visualize.graph_visualize(name,json_data):
+        if graph_visualize.graph_visualize(name, json_data):
             print(f"Visualization of the NFA is saved in {name}")
         else:
             print("Error: Visualization failed")
         del graph_visualize
+
+    # getter for nfs json
+    def get_nfa_json(self):
+        return self._nfa_json
+
 
 regex = "[A-Za-z]+[0-9]*"
 # regex = "ab*c+de?(f|g|h)|mr|n|[p-qs0-9]"
